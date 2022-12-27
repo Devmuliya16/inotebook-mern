a9ad7fdb8c9d76f4d4b307a563f1e1d2dc4e5f72
Devmuliya16 <muliyadev16@gmail.com>
2022-12-27 22:10:49 +0530

    ldn

--- index.js
+++ index.js
@@ -27,8 +27,8 @@ app.use('/', require('./routes/notes'));
 //if environment process NODE_ENV is production then use buid virson of the frontend
 if(process.env.NODE_ENV === "production"){
     app.use(express.static('./frontend/build'));
-    app.get('/',(req,res)=>{
-        res.sendFile('../frontend/build/index.js',(err)=>{if(err){res.send(err)}})
+    app.get('/*',(req,res)=>{
+        res.sendFile('./frontend/build/index.js',(err)=>{if(err){res.send(err)}})
     });
 }
 
